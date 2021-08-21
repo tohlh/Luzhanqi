b239dd1485a05a49dfb9e51409a228b1499e574a
tohlh <tohliheng@gmail.com>
2021-08-21 15:24:49 +0800

    completed move validation

--- validator.cpp
+++ validator.cpp
@@ -17,10 +17,14 @@ bool Validator::checkMove(ChessPiece* chess, int x, int y)
     int currX = chess->getXCoord();
     int currY = chess->getYCoord();
 
-    if ((abs(x - currX) == 1 && abs(y - currY) == 0) || (abs(x - currX) == 0 && abs(y - currY) == 1)) { // strictly vertical/horizontal
+    if (((abs(x - currX) == 1 && abs(y - currY) == 0) || (abs(x - currX) == 0 && abs(y - currY) == 1)) &&
+         ((currY <= 6 && y <= 6) || (currY >= 7 && y >= 7))) { // strictly vertical/horizontal, only applies on one side
         return true;
-    } else if ((abs(x - currX) == 1 && abs(y - currY) == 1)) { // one step diagonals
+    } else if (((abs(x - currX) == 1 && abs(y - currY) == 1)) &&
+               ((currY <= 6 && y <= 6) || (currY >= 7 && y >= 7))) { // one step diagonals, only applies on one side
         return checkOneStepDiagonal(currX, currY, x, y);
+    } else if (((currY == 6 && y == 7) || (currY == 7 && y == 6)) && currX == x) {
+        return (x == 1 || x == 3 || x == 5);
     } else { // multisteps
         bool c1 = currY == 2 || currY == 6 || currY == 7 || currY == 11;
         bool c2 = (currX == 1 || currX == 5) && (currY != 1 && currY != 12);
@@ -32,19 +36,57 @@ bool Validator::checkMove(ChessPiece* chess, int x, int y)
         bool c6 = x == 3 && (y == 6 || y == 7);
         if (!(c4 || c5 || c6)) return false; // destination not on track
 
-        bool c7 = (currX == x) || (currY == y);
+        bool c7 = chess->getChessType() == gongbing;
         if (c7) {
-            return checkMultistepStraights(currX, currY, x, y);
-        } else {
             return checkMultistepFree(currX, currY, x, y);
+        } else {
+            if (((currX == x) && (currX == 1 || currX == 5)) ||
+                ((currY == y) && (currY == 2 || currY == 6 || currY == 7 || currY == 11)))
+            {
+                return checkMultistepStraights(currX, currY, x, y);
+            } else {
+                return false;
+            }
         }
     }
     return false;
 }
 
-int Validator::checkAttack(ChessPiece* chess1, ChessPiece* chess2)
+int Validator::checkAttack(ChessPiece* chess1, ChessPiece* chess2) // 0 chess1 destroyed, 1 chess2 destroyed, -1 both destroyed, -2 invalid move
 {
+    if (chess1->getChessColor() == chess2->getChessColor()) { // kill own teammate lol?
+        return -2;
+    }
 
+    int x2 = chess2->getXCoord();
+    int y2 = chess2->getYCoord();
+
+    if (!checkMove(chess1, x2, y2)) { // out of reach or chess1 cannot move
+        return -2;
+    }
+
+    if (junying.contains({x2, y2})) { // chess2 is protected
+        return -2;
+    }
+
+    if (chess1->getChessType() == zhadan || chess2->getChessType() == zhadan) {
+        return -1;
+    }
+
+    if (chess1->getChessType() == gongbing && chess2->getChessType() == dilei) {
+        return 1;
+    }
+
+    if (chess1->getChessType() >= gongbing && chess1->getChessType() <= siling &&
+        chess2->getChessType() >= gongbing && chess2->getChessType() <= siling) {
+        if (chess1->getChessType() > chess2->getChessType()) {
+            return 1;
+        } else if (chess1->getChessType() == chess2->getChessType()) {
+            return -1;
+        }
+    }
+
+    return -2;
 }
 
 bool Validator::checkOneStepDiagonal(int currX, int currY, int x, int y)
@@ -86,6 +128,16 @@ bool Validator::checkOneStepDiagonal(int currX, int currY, int x, int y)
 
 bool Validator::checkMultistepStraights(int currX, int currY, int x, int y)
 {
+    bool c1 = currY == 6;
+    bool c2 = currY == 7;
+    bool c3 = y == 6;
+    bool c4 = y == 7;
+    bool c5 = currX == 1 || currX == 3 || currX == 5;
+
+    if (((c1 && c4) || (c2 && c3)) && (c5)) {
+        return true;
+    }
+
     if (currX == x) {
         int smallY = y < currY ? y : currY;
         int bigY = y > currY ? y : currY;
@@ -109,6 +161,47 @@ bool Validator::checkMultistepStraights(int currX, int currY, int x, int y)
 
 bool Validator::checkMultistepFree(int currX, int currY, int x, int y)
 {
+    QList < QPair <int, int> > vis;
+    vis.append({currX, currY});
 
-    return false;
+    bool c1 = searchUtil(vis, currX + 1, currY, x, y);
+    bool c2 = searchUtil(vis, currX - 1, currY, x, y);
+    bool c3 = searchUtil(vis, currX, currY + 1, x, y);
+    bool c4 = searchUtil(vis, currX, currY - 1, x, y);
+
+    return (c1 || c2 || c3 || c4);
+}
+
+bool Validator::searchUtil(QList < QPair <int, int> > &vis, int currX, int currY, int x, int y)
+{
+    if (currX == x && currY == y) {
+        return true;
+    }
+
+    if (currX > 5 || currX < 1 || currY > 12 || currY < 1) {
+        return false;
+    }
+
+    bool c1 = currY == 2 || currY == 6 || currY == 7 || currY == 11;
+    bool c2 = (currX == 1 || currX == 5) && (currY != 1 && currY != 12);
+    bool c3 = currX == 3 && (currY == 6 || currY == 7);
+    if (!(c1 || c2 || c3)) {
+        return false;
+    }
+
+    if (vis.contains({currX, currY})) {
+        return false;
+    }
+    vis.append({currX, currY});
+
+    if (grids[currY - 1][currX - 1]->getOccupied()) {
+        return false;
+    }
+
+    bool c5 = searchUtil(vis, currX + 1, currY, x, y);
+    bool c6 = searchUtil(vis, currX - 1, currY, x, y);
+    bool c7 = searchUtil(vis, currX, currY + 1, x, y);
+    bool c8 = searchUtil(vis, currX, currY - 1, x, y);
+
+    return (c5 || c6 || c7 || c8);
 }
--- validator.h
+++ validator.h
@@ -9,12 +9,13 @@ class Validator
 public:
     Validator(QList < QList <Grid*> > &g);
     bool checkMove(ChessPiece* chess, int x, int y); //Check if the chess can go from its current pos to desired pos
-    int checkAttack(ChessPiece* chess1, ChessPiece* chess2); // 0 chess1 destroyed, 1 chess2 destroyed, -1 both destroyed
+    int checkAttack(ChessPiece* chess1, ChessPiece* chess2); // 0 chess1 destroyed, 1 chess2 destroyed, -1 both destroyed, -2 invalid move
 
 private:
     bool checkOneStepDiagonal(int currX, int currY, int x, int y);
     bool checkMultistepStraights(int currX, int currY, int x, int y);
     bool checkMultistepFree(int currX, int currY, int x, int y);
+    bool searchUtil(QList < QPair <int, int> > &vis, int currX, int currY, int destX, int destY);
     QList < QList <Grid* > > grids;
     QList < QPair <int, int> > junying;
 };
