8cc03466dd6056873aac84b361ed3fc4c65b2ffa
tohlh <tohliheng@gmail.com>
2021-08-26 14:24:28 +0800

    fix IP validation

--- client.cpp
+++ client.cpp
@@ -26,9 +26,9 @@ void Client::on_connectButton_clicked()
     serverIP = ui->userInputIP->text();
     serverPort = ui->userInputPort->text().toInt();
 
-    QRegularExpression ipFormat("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}$");
+    QRegularExpression ipFormat("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
 
-    if (!ipFormat.match(serverIP).hasMatch()) {
+    if (!ipFormat.match(serverIP).hasMatch() && serverIP != "localhost") {
         ui->notifcationLabel->setText("Invalid IP address");
         return;
     }
